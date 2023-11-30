ecb9b1ea8c31ce09e60fcb9ea4b4af0abe7be99e
Cristina Nicolàs <cristina.nicolas@clipplatform.com>
2023-11-30 18:43:21 +0100

    Instalacion Docker

---
+++ Documentación/InstalacionDockerBC.ps1
@@ -0,0 +1,5 @@
+﻿##Install-Module -Name BcContainerHelper -Force
+
+$artifactUrl = Get-BCArtifactUrl -country es -select Latest -type Sandbox
+
+New-BcContainer -accept_eula -auth UserPassword -containerName BC23CU1ES -artifactUrl $artifactUrl -includeTestToolkit -includeTestLibrariesOnly -updateHosts -useBestContainerOS -dns '8.8.8.8'
